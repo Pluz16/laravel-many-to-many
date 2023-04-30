5c0515866ee6995dcbae81de2e75d036c3d953b3
Francesco Punta <puntafrancesco@gmail.com>
2023-04-30 14:31:55 +0200

    Create ed update

---
+++ resources/views/layouts/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Create Project</h1>
+
+    <form action="{{ route('projects.store') }}" method="post">
+        @csrf
+
+        @include('error_messages')
+
+        <div class="form-group">
+            <label for="name">Project Name</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="user">User</label>
+            <input type="text" class="form-control" id="user" name="user" value="{{ old('user') }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description" rows="3">{{ old('description') }}</textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="url">URL</label>
+            <input type="url" class="form-control" id="url" name="url" value="{{ old('url') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="slug">Slug</label>
+            <input type="text" class="form-control" id="slug" name="slug" value="{{ old('slug') }}" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create Project</button>
+    </form>
+</div>
+@endsection
---
+++ resources/views/layouts/update.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Edit Project</h1>
+
+    <form action="{{ route('projects.update', $project->id) }}" method="post">
+        @csrf
+        @method('PUT')
+
+        @include('error_messages')
+
+        <div class="form-group">
+            <label for="name">Project Name</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $project->name) }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="user">User</label>
+            <input type="text" class="form-control" id="user" name="user" value="{{ old('user', $project->user) }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description" rows="3">{{ old('description', $project->description) }}</textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="url">URL</label>
+            <input type="url" class="form-control" id="url" name="url" value="{{ old('url', $project->url) }}">
+        </div>
+
+        <div class="form-group">
+            <label for="slug">Slug</label>
+            <input type="text" class="form-control" id="slug" name="slug" value="{{ old('slug', $project->slug) }}" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Update Project</button>
+    </form>
+</div>
+@endsection
