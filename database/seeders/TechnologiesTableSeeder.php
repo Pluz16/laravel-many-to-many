cfadd9e193d3223b9431788beb20f81190b7cc56
Francesco Punta <puntafrancesco@gmail.com>
2023-05-08 15:33:59 +0200

    Technologies list

--- database/seeders/TechnologiesTableSeeder.php
+++ database/seeders/TechnologiesTableSeeder.php
@@ -1,19 +1,21 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Technology;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TechnologiesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $technologies = ['CSS', 'JS', 'Vue', 'SQL', 'PHP', 'Laravel'];
+
+        foreach ($technologies as $technology) {
+            Technology::create([
+                'name' => $technology
+            ]);
+        }
     }
 }
